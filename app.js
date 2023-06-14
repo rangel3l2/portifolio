10f9e0c8e80d084c7212fe8c2ff720c896974b75
rangel3l2 <rangel.silva@estudante.ifms.edu.br>
2023-06-14 17:51:18 -0400

    update apperances

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 const menu = document.getElementById('menu'); 
 const menuUl = document.getElementById('menu-ul');
 const menuUlLi = document.querySelector('.menuUlLi')
-
+const header = document.getElementById('header');
 let toogle;
+
+const darkMode = document.getElementsByClassName('dark-mode');
 const container = document.getElementById('container');
 const noturnMode = document.querySelector('#noturn-mode-div');
 //noturn mode 
-const blackColor = '#3b2a2a'
-const primaryColor = '#b11b1b'
+const blackColor = '#383a52'
+const primaryColor = '#f7f3f3'
+const textColor = '#b11b1b';
 let modeSky =  true
 function toggleSky(){
     
     if(noturnMode.classList == 'moon')
     { 
         modeSky  = false
+        for(let i = 0;i < darkMode.length;i++){
+          darkMode[i].style.color = primaryColor
+        }
+        
         noturnMode.classList.remove('moon')
         noturnMode.classList.add('sun')
         container.style.backgroundColor = blackColor
+        
     }
     else
     { 
@@ -24,38 +32,37 @@ function toggleSky(){
         noturnMode.classList.remove('sun')
         noturnMode.classList.add('moon')
         container.style.backgroundColor = primaryColor
+        for(let i = 0;i < darkMode.length;i++){
+          darkMode[i].style.color = 'inherit'
+        }
     }
     
 
 }
 
 function toggleMenu(){
-    console.log(modeSky)
+
   toggle = true;
   if(toggle){
-    menuUl.style.listStyle = 'none';
+    
     /* document.getElementsByTagName('ul').style.listStyle = 'none';  */ 
     menu.style.display = 'block';
     menu.style.listStyle = 'none';
     toggle = false;
-
     container.style.backgroundColor = blackColor
     menu.style.backgroundColor = primaryColor
     
   }
   menu.onmouseleave = function() {
+
     menu.style.display = 'none';
     if(modeSky)container.style.backgroundColor = primaryColor
+  }    
+
   }
-  
-
-
-    
-
-}
 function closeMenu(){
     if(!toggle){
-        menuUl.style.listStyle = 'none';
+        
         /* document.getElementsByTagName('ul').style.listStyle = 'none';  */ 
         menu.style.display = 'none';
         menu.style.listStyle = 'none';
@@ -65,14 +72,36 @@ function closeMenu(){
     
       }  
  }
+ const title = document.querySelector('.title')
  var scrollval = 0;
  window.addEventListener('scroll', () => {
    if(scrollval > window.scrollY) {
-     // Do something
-     console.log('Scroll up')
+     // scroll up
+        if(scrollval < 50){
+            title.innerHTML = ''
+            const textTitle = document.createElement('p')
+            textTitle.innerText = 'Rangel Gomes'
+            title.appendChild(textTitle)
+        }
+     
    } else {
-     // Do something
-     console.log('Scroll down')
+     // scrooldown
+     
+        if(scrollval >= 50){
+            title.innerHTML = ''
+            const imgR = document.createElement('img')
+            imgR.setAttribute('src','img/r.png')
+            imgR.id = 'imgR'
+            title.appendChild(imgR)
+            header.style.position = 'fixed'
+            header.style.width = '100%'
+            header.style.backgroundColor = primaryColor
+            header.style.height =  '10vh'
+        
+        
+     }
+     
+
    }
    scrollval = window.scrollY;
  });
