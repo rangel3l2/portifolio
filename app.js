897287fef50cab90c94becf5d515f5b18ef237a5
rangel3l2 <rangel.silva@estudante.ifms.edu.br>
2024-07-20 00:15:55 -0400

    upd many chages

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ const header = document.getElementById('header');
 let toogle;
 
 const darkMode = document.getElementsByClassName('dark-mode');
-const container = document.getElementById('container');
+const container = document.querySelector('.container');
 const noturnMode = document.querySelector('#noturn-mode-div');
 const menuIdiom = document.getElementById('menu-idiom')
+const menuIdiomUl = document.querySelectorAll('#menu-idiom ul li')
+const content = document.getElementById('content-general')
+const portuguese = document.getElementById('portuguese')
+const english = document.getElementById('english')
 //noturn mode 
 const blackColor = '#3b2a2a'
 const primaryColor = '#f7f3f3'
@@ -27,24 +31,35 @@ function toggleSky(){
         
         noturnMode.classList.remove('moon')
         noturnMode.classList.add('sun')
-        container.style.backgroundColor = blackColor
-        header.style.backgroundColor = blackColor     
-        header.style.borderBottom = `2px solid ${secundaryColor}`
-        menuIdiom.style.background = blackColor;
-      
+       
+        menuIdiom.classList.remove('white-color')
+        menuIdiom.classList.add('black-color')
+        menuIdiomUl.forEach(element => {
+          element.style.color = blackColor
+        });
+        
+        header.classList.remove('header-white')
+        header.classList.add('header-dark')
+        container.classList.remove('white-color')
+        container.classList.add('black-color')
+       
+        
     }
     else
     { 
         modeSky = true
         noturnMode.classList.remove('sun')
         noturnMode.classList.add('moon')
-        container.style.backgroundColor = primaryColor
+        container.classList.remove('black-color')
+        container.classList.add('white-color')
         for(let i = 0;i < darkMode.length;i++){
           darkMode[i].style.color = 'inherit'
         }
-        header.style.backgroundColor = secundaryColor     
-        header.style.borderBottom = `none`
-        menuIdiom.style.background = primaryColor;
+        header.classList.remove('header-dark')
+        header.classList.add('header-white')    
+       
+        menuIdiom.classList.remove('black-color')
+        menuIdiom.classList.add('white-color')
     }
     
 
@@ -128,3 +143,46 @@ function closeMenu(){
     
   }
  }
+ let toggleIdiomMenu = false
+ let previousToggleIdiomMenu = toggleIdiomMenu;
+ document.getElementById('portuguese').addEventListener('click', function() {
+  toggleIdiomMenu = false
+  Weglot.switchTo('pt');
+}, false);
+document.getElementById('english').addEventListener('click', function() {
+  Weglot.switchTo('en');
+  toggleIdiomMenu = true
+}, false);
+
+
+
+
+setInterval(function() {
+  if (toggleIdiomMenu !== previousToggleIdiomMenu) {
+      console.log('toggleIdiomMenu changed:', toggleIdiomMenu);
+      previousToggleIdiomMenu = toggleIdiomMenu;
+      if(toggleIdiomMenu){
+        portuguese.style.color = 'black'
+        english.style.color = 'red'
+      }
+      else{
+        portuguese.style.color = 'red'
+        english.style.color = 'black'
+      }
+  }
+}, 1000);
+
+function scrollDown(){
+  window.scrollTo(0, 1);
+
+
+
+ 
+  alert('scroll down')
+}
+window.addEventListener('scroll', function() {
+  // Verifica se a rolagem vertical é maior que 100px (ajuste conforme necessário)
+  if (window.scrollY > 10) {
+      scrollDown();
+  }
+});
